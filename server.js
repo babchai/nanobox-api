0c4d9f6a4b5d57e8a2c1e17ef368a7fd788a9d2c
babchai <babchai@hotmail.com>
2019-06-26 00:37:51 +0700

    a

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var net = require('net');
 var url = require('url');
 var _ = require('lodash');
 const hex2ascii = require('hex2ascii')
+var conv = require('binstring');
+
 
 const arrayBufferToHex = require('array-buffer-to-hex')
 
@@ -47,8 +49,9 @@ server.on('connection', function(socket) { //This is a standard net.Socket
             
           
            var newHex =  Buffer.from(body, 'utf8').toString('hex');
-
-            socket.sendMessage(newHex);
+           var newBin = conv(body, { out:'bytes' })
+            socket.sendMessage(newBin);
+           // socket.write(newBin);
             //socket.sendMessage(bodyBuff);
 
             // var bodyBuff = Buffer.from(JSON.stringify(successResp));
