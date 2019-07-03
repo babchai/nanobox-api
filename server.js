471ac10b34a53915f0195e106b226efde1a3ef8a
babchai <babchai@hotmail.com>
2019-07-04 01:32:23 +0700

    update

--- server.js
+++ server.js
@@ -38,10 +38,20 @@ server.on('connection', function(socket) { //This is a standard net.Socket
             let dataJson = JSON.stringify(ascii);
             console.log("json stringify ==> " , dataJson);
             
-            var successResp = { "code":0,"msg":"connect success","data":{}}
+            //console.log("data ==> " , dataJson.data)
+            if(dataJson.data){
+                var successResp = { "code":0,"msg":"connect success","data":{}}
+                var body = '{"code":0,"msg":"connect success","data":{}}'
+                sockets.push({'soc':socket , 'data' : dataJson.data})
+
+            }
+            else {
+                var successResp = { "code": 0,"msg": "","data":{}}
+                var body = '{"code": 0,"msg": "","data":{}}'
+            }
             var length = Object.keys(successResp).length +2;
             console.log(length);
-            var body = '{"code":0,"msg":"connect success","data":{}}'
+            
             
             var newBuff = new Buffer(4)
             var b = Buffer.from(body , 'utf-8');
@@ -58,7 +68,6 @@ server.on('connection', function(socket) { //This is a standard net.Socket
             console.log("send back bin" , buff );
             socket.write(buff);
 
-            sockets.push({'soc':socket , 'data' : dataJson.data})
 
         }catch(e){
             console.log("error : " , e);
